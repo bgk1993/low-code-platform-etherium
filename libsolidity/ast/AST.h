aarlt/solidity

Basic nlohmann-json integration.

libsolidity/ast/AST.h
@@ -22,7 +22,6 @@
 
 #pragma once
 
-
 #include <libsolidity/ast/ASTForward.h>
 #include <libsolidity/parsing/Token.h>
 #include <libsolidity/ast/Types.h>
@@ -33,7 +32,7 @@
 #include <libevmasm/Instruction.h>
 
 #include <libdevcore/FixedHash.h>
-#include <json/json.h>
+#include <libdevcore/JSON.h>
 
 #include <boost/noncopyable.hpp>
 

bgk1993/low-code-platform-etherium

Co-authored-by: Alex Beregszaszi <alex@rtfs.hu>

libsolidity/ast/AST.h
@@ -32,10 +32,9 @@
 #include <liblangutil/SourceLocation.h>
 #include <libevmasm/Instruction.h>
 #include <libsolutil/FixedHash.h>
+#include <libsolutil/JSON.h>
 #include <libsolutil/LazyInit.h>
 #include <libsolutil/Visitor.h>
-
-#include <json/json.h>
 
 #include <range/v3/view/subrange.hpp>
 #include <range/v3/view/map.hpp>