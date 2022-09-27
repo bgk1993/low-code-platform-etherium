5847c5c7d73d8683f56f3d7d237f86d2fb17b9e7
wechman <damian.wechman.it@gmail.com>
2022-09-27 08:12:25 +0200

    Fix multiple user-defined functions provided for operator test case.

--- test/libsolidity/syntaxTests/using/two_functions_bound_to_the_same_unary_operator_as_global_and_non_global.sol
+++ test/libsolidity/syntaxTests/using/two_functions_bound_to_the_same_unary_operator_as_global_and_non_global.sol
@@ -3,17 +3,13 @@ type Int is int;
 using {unsub as -} for Int global;
 using {another_unsub as -} for Int;
 
-function unsub(Int, Int) pure returns (Int) {
-    return Int.wrap(0);
-}
+function unsub(Int) pure returns (Int) {}
 
-function another_unsub(Int, Int) pure returns (Int) {
-    return Int.wrap(0);
-}
+function another_unsub(Int) pure returns (Int) {}
 
 function test() pure returns (Int) {
     return -Int.wrap(2);
 }
 
 // ----
-// TypeError 4907: (292-304): Built-in unary operator - cannot be applied to type Int. No matching user-defined operator found.
+// TypeError 4907: (232-244): Built-in unary operator - cannot be applied to type Int. Multiple user-defined functions provided for this operator.
