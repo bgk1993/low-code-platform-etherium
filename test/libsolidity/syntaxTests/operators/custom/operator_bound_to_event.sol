9e3a1f48e97437916e6182cd55d26891090c1441
wechman <damian.wechman.it@gmail.com>
2022-09-27 08:06:16 +0200

    Fix operator bound to event test

--- test/libsolidity/syntaxTests/operators/custom/operator_bound_to_event.sol
+++ test/libsolidity/syntaxTests/operators/custom/operator_bound_to_event.sol
@@ -4,10 +4,7 @@ contract C {
     event Event();
 }
 
-
-function add(C.Event, C.Event) pure returns (C.Event) {
-    return C.Event.E1;
-}
+function add(C.Event, C.Event) pure returns (C.Event) {}
 
 // ----
 // TypeError 5172: (21-28): Name has to refer to a user-defined value type, struct, enum or contract.
